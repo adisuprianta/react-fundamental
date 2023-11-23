01af3b03f4f3d52e5fc7fa9f60de476c0c5abdd4
Muhamad Rifqi Ramadhan <rifqyomp@gmail.com>
2023-11-23 09:27:49 +0700

    Belajar Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
+import Login from './pages/Login';
 
 export default function App() {
   return (
-    <>
-      <Navbar />
-      <Home />
-    </>
+    <div>
+      <Login />
+    </div>
   );
 }
